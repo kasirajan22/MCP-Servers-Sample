28ef788526f378bc3b483f8fcad0f36c1c8a149b
kasirajan2289 <kasirajan22@hotmail.com>
2025-06-13 10:44:27 +0530

    Add project files.

---
+++ MCP/Program.cs
@@ -0,0 +1,36 @@
+﻿using MCP;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<MonkeyService>();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+    [McpServerTool, Description("Return the message")]
+    public static string Return(string message) => new string(message);
+}
+
